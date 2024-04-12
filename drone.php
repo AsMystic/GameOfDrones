2dbd929c509fec88dfc4ae19d3691c9c44395c6c
AsMystic <155950094+AsMystic@users.noreply.github.com>
2024-04-12 15:56:24 +0200

    Commit final

--- drone.php
+++ drone.php
@@ -11,13 +11,16 @@
 
       if (isset($_SESSION['login'])) {
         $login = $_SESSION['login'];
-        echo "<p><b>Bienvenue " . $login . " !</b></p>";
+        echo "<p class='title'>Bienvenue " . $login . " !</p>";
       }
     ?>
-    <a href="afficher_drones.php" class="button">Afficher les drones</a>
-    <a href="ajouter_drones.php" class="button">Ajouter un drone</a>
-    <a href="login.php" class="button_login">Déconnexion</a>
-  </div>
+
+	<a href="afficher_drones.php"><button class="button">Liste des drones</button></a>
+</div>
+	<a href="ajouter_drones.php"><button class="button">Ajouter un drone</button></a>
+</div>
+	<a href="login.php"><button class="buttondeco" style="vertical-align:middle"><span>Quitter </span></button></a>
+</div>
 </body>
 
 </html>
